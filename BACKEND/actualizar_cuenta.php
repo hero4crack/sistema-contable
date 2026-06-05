8dfc94a0407c069d712fb412dcdd9bd858fc9c83
Vurger <miguelnavarrof2002@gmail.com>
2026-06-04 21:27:41 -0400

    cuentas

--- BACKEND/actualizar_cuenta.php
+++ BACKEND/actualizar_cuenta.php
@@ -3,16 +3,15 @@ require_once 'conecxion_bd.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Capturamos los campos exactos del Catálogo
-    $id = $_POST['id_cuenta'];
-    $codigo = $_POST['codigo_cuenta'];
-    $nombre = $_POST['nombre_cuenta'];
+    $id = $_POST['id'];
+  
+    $nombre = $_POST['cuenta'];
     $tipo = $_POST['tipo'];
     $nivel = $_POST['nivel'];
     $movimiento = $_POST['movimiento']; // Aquí recibimos el valor (1 o 0)
 
     // UPDATE exclusivo para la tabla de cuentas
     $sql = "UPDATE catalogo_cuentas SET 
-            codigo_cuenta = '$codigo', 
             nombre_cuenta = '$nombre', 
             tipo_cuenta = '$tipo', 
             nivel = '$nivel', 
